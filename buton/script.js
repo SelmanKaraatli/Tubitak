1d27583f927624ffe75bc91c10bd6ce776096459
SelmanKaraatli <emirselmankaraatli@gmail.com>
2020-12-19 21:44:27 +0300

    19.12.2020

---
+++ buton/script.js
@@ -0,0 +1,19 @@
+window.onload=function(){
+    document.getElementById("kirmizi").addEventListener("click", Kirmizi);
+    document.getElementById("yesil").addEventListener("click", Yesil);
+    document.getElementById("mavi").addEventListener("click", Mavi);
+    document.getElementById("monokromasi").addEventListener("click", Monokromasi);
+
+    function Kirmizi() {
+        console.log('Hello!');
+    }
+    function Yesil() {
+        console.log('Hello!');
+    }
+    function Mavi() {
+        console.log('Hello!');
+    }
+    function Monokromasi() {
+        console.log('Hello!');
+    }
+}
